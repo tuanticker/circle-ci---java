9fb19d7b049a9d6d1d027dec0d6e8969aea26555
Phạm Tuấn Anh <anhpt@nodo.vn>
2019-07-12 11:05:11 +0700

    first commit

---
+++ src/main/java/com/example/demo/ApiController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/")
+public class ApiController {
+
+    @GetMapping("version")
+    public String version() {
+      return "11.2.3";
+    }
+
+}
